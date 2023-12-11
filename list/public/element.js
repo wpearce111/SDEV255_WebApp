c3a995de751097fe35a867822f7042a0f53e1f37
wpearce111 <wpearce1@ivytech.edu>
2023-12-11 18:17:45 -0500

    Initial Commit
    Added list folder to root

---
+++ list/public/element.js
@@ -0,0 +1,57 @@
+const Element = {
+    data() {
+      return {
+        inputTitle : false,
+        inputTask : false,
+        inputDateA : false
+      }
+    },
+    template : `
+      <li> 
+        <button @click="remove()"> Remove </button> 
+
+        <span v-if="!inputTitle"> {{element.text}} </span>
+        <input v-else type="text" :value="element.text" @blur="modify($event)" ref="refInputTitle"/>
+        <button @click="inputTitle=true"> Modify Task Title </button>
+
+        <span v-if="!inputTask"> {{element.task}} </span>
+        <input v-else type="text" :value="element.task" @blur="task($event)" ref="refInputTask"/>
+        <button @click="inputTask=true"> Modify Task Description</button>
+        
+        <span v-if="!inputDateA"> {{element.dateA}} </span>
+        <input v-else type="text" :value="element.dateA" @blur="dateA($event)" ref="refInputDateA"/>
+        <button @click="inputDateA=true"> Modify Date Assigned </button>
+      </li>
+    `,
+    props : ["element"],
+    methods : {
+      remove() {
+        // process the click on the Remove button
+        this.$emit("remove", { id : this.element._id });
+      },
+      modify(event) {
+        var value = event.target.value;
+        this.inputTitle = false;
+        this.$emit("modify", { id : this.element._id, value : value });
+      },
+      task(event) {
+        var value = event.target.value;
+        this.inputTask = false;
+        this.$emit("task", { id : this.element._id, value : value });
+      },
+      dateA(event) {
+        var value = event.target.value;
+        this.inputDateA = false;
+        this.$emit("dateA", { id : this.element._id, value : value });
+      }
+    },
+    emits : ["remove", "modify", "task", "dateA"],
+    updated() {
+      // check that refInputTitle exists, and if so, give focus to 
+      // the input field
+      if (this.$refs.refInputTitle) this.$refs.refInputTitle.focus();  
+      if (this.$refs.refInputTask) this.$refs.refInputTask.focus(); 
+      if (this.$refs.refInputDateA) this.$refs.refInputDateA.focus();  
+    }
+  }
+  export default Element;
---
+++ list/public/global-app.js
@@ -0,0 +1,124 @@
+import Element from "./element.js";
+
+const GlobalApp = {
+  data() {
+    return {
+      elements: [],
+    };
+  },
+  components: {
+    Element: Element,
+  },
+  template: `
+    <button @click="add()">Add Task</button>
+    <ul>
+      <Element v-for="(element, index) in elements" 
+       :key="index" :element="element"
+        @remove="remove($event)" @modify="modify($event)" @task="task($event)" @dateA="dateA($event)"
+      />
+    </ul>
+  `,
+  methods: {
+    async add() {
+      var defaultTitle = "Element " + (this.elements.length + 1);
+      var defaultTaskDescription = "Enter task description";
+      var defaultDateText = "Enter date";
+      try {
+
+        const response = await axios.post("/list", { 
+          text: defaultTitle, 
+          task: defaultTaskDescription,
+          dateA: defaultDateText 
+        });
+
+        console.log(this.elements);
+        this.elements.push({ 
+          _id: response.data.id,
+          text: defaultTitle, 
+          task: defaultTaskDescription,
+          dateA: defaultDateText
+        });
+
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    remove(params) {
+      var id = params.id;
+      // remove the element with this id from the elements
+      // array
+      this.elements = this.elements.filter(function (element) {
+        if (element._id == id) return false;
+        else return true;
+      });
+      axios.delete("/list", { data : {id:id} });    
+            // the options must be written in the data 
+            // property
+    },
+    modify(params) {
+      var id = params.id;
+      var value = params.value;
+      // modify the title of the element with this id in the
+      // elements array
+
+      this.elements = this.elements.map(function (element) {
+        if (element._id == id) {
+          element.text = value;
+          return element;
+        } else return element;
+      });
+      // modify the text of the element having this 
+      // identifier
+      axios.put("/list", {text:value, id:id});
+    },
+    task(params) {
+      var id = params.id;
+      var value = params.value;
+      // modify the task of the element with this id in the
+      // elements array
+
+      this.elements = this.elements.map(function (element) {
+        if (element._id == id) {
+          element.task = value;
+          return element;
+        } else return element;
+      });
+      // modify the text of the element having this 
+      // identifier
+      axios.put("/list", {id:id, task:value});
+    },
+    dateA(params) {
+      var id = params.id;
+      var value = params.value;
+      // modify the dateAssigned of the element with this id in the
+      // elements array
+
+      this.elements = this.elements.map(function (element) {
+        if (element._id == id) {
+          element.dateA = value;
+          return element;
+        } else return element;
+      });
+      // modify the text of the element having this 
+      // identifier
+      axios.put("/list", {id:id, dateA:value});
+    }
+  },
+  async created() {
+    try {
+      const response = await axios.get("/list");
+      this.elements = response.data.elements.map(function (element) {
+        return { 
+          _id: element._id, 
+          text: element.text , 
+          task: element.task,
+          dateA: element.dateA
+        };
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
+export default GlobalApp;
